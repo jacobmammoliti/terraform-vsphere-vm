0f15171d9b1d6c0d820672b78c640dd7a5da19d3
Administrator <admin@example.com>
2020-01-15 08:27:19 -0600

    initial deployment of vault and kubernetes

--- examples/vault.tf
+++ examples/vault.tf
@@ -17,3 +17,18 @@ module "vault-servers" {
   folder            = "jacob"
   data_disk_size_gb = [25]
 }
+
+module "vault-servers" {
+  source  = "tfe.cdcug.local/arctiq/demo/vmware"
+  version = "0.0.1"
+
+  datastore         = "CDCUG_VMware_general"
+  cluster           = "cluster01"
+  network           = "VM Network"
+  datacenter        = "cdcug"
+  template          = "centos-7.7-template"
+  domain            = "cdcug.local"
+  vmname            = "kubernetes"
+  instances         = 2
+  folder            = "jacob"
+}

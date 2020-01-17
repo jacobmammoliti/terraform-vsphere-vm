4410e405cac99f40af7ecc23edbf11937ff79aeb
Administrator <admin@example.com>
2020-01-17 13:26:47 -0600

    creating a k8s node

---
+++ examples/kubernetes.tf
@@ -0,0 +1,18 @@
+module "kubernetes" {
+  source  = "tfe.cdcug.local/arctiq/vm/vsphere"
+  version = "0.0.2"
+
+  datastore         = "CDCUG_VMware_general"
+  cluster           = "cluster01"
+  network           = "VM Network"
+  datacenter        = "cdcug"
+  template          = "centos-7.7-template2"
+  domain            = "cdcug.local"
+  vmname            = "kubernetes"
+  instances         = 1
+  folder            = "jacob"
+}
+
+output "kubernetes_ips" {
+  value = module.kubernetes.instance_ip_addr
+}

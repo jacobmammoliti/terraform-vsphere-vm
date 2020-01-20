70b3e16316e2cc767e58a277a3c39d89eb5a3524
Administrator <admin@example.com>
2020-01-20 08:16:43 -0600

    add k8s file

---
+++ examples/kubernetes/kubernetes.tf
@@ -0,0 +1,22 @@
+provider "vsphere" {
+ allow_unverified_ssl = true
+}
+
+module "kubernetes" {
+  source  = "tfe.cdcug.local/arctiq/vm/vsphere"
+  version = "0.0.2"
+
+  datastore         = "CDCUG_VMware_general"
+  cluster           = "cluster01"
+  network           = "VM Network"
+  datacenter        = "cdcug"
+  template          = "centos-7.7-template2"
+  domain            = "cdcug.local"
+  vmname            = "kubernetes"
+  instances         = 1
+  folder            = "jacob"
+}
+
+output "kubernetes_ips" {
+  value = module.kubernetes.instance_ip_addr
+}

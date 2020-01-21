be2f82acf9ee999129f28fec7bc475f8fead830c
Administrator <admin@example.com>
2020-01-21 14:32:49 -0600

    delete k8s node

--- examples/kubernetes/kubernetes.tf
+++ examples/kubernetes/kubernetes.tf
@@ -13,7 +13,7 @@ module "kubernetes" {
   template          = "centos-7.7-template2"
   domain            = "cdcug.local"
   vmname            = "kubernetes"
-  instances         = 1
+  instances         = 0
   folder            = "jacob"
 }
 
